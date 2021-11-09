66ea83b58423a40e10c2336d87948814784612d9
Cristian <mejia.cristian583@gmail.com>
2021-11-08 23:33:37 -0600

    commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Importamos las configuraciones iniciales
-# from website import create_app as application
+from website import create_app as application
 from website import create_app
 application = create_app()
 
